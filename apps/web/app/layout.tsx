fe63840a1d11c808c1c6ffd7afa7fab4e5996117
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-03-08 16:35:34 +0100

    Migrate app to new app directory api.

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "React";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
