60bc80a81ebbc3dd1b582e6e59d7343d20ed3783
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-03-08 17:48:00 +0100

    Add header and logo components.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "ui";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function Homepage() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <Button />
       <Analytics />
     </div>
   );
