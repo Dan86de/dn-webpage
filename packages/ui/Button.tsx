a9da2f8864756615fcc357cb37e82543c5ddb082
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-03-08 17:33:33 +0100

    Setup tailwindcss global config.

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+
 export const Button = () => {
-  return <button>Boop</button>;
+  return (
+    <button className="mx-auto block bg-black text-white py-2 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-400">
+      Button
+    </button>
+  );
 };
