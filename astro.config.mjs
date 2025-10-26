f4e824b47de41d787bacd11abe48a6a19da38d5f
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-10-26 12:54:49 +0100

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imagesConfig: {
+      sizes: [480, 744, 1024, 1440],
+    },
+    imageService: true,
+    devImageService: "sharp",
+    isr: true,
+  }),
 });
