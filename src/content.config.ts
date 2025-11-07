48ee451cebb4cb32f5a69be921fd24beb462d690
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-11-07 20:43:28 +0100

    Add content collection for blog

---
+++ src/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from "astro:content";
+
+import { glob, file } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    isDraft: z.boolean(),
+    title: z.string(),
+    // image: z.object({
+    //   src: z.string(),
+    //   alt: z.string(),
+    // }),
+    author: z.string().default("Anonymous"),
+    tags: z.array(z.string()),
+
+    // In YAML, dates written without quotes around them are interpreted as Date objects
+    publishDate: z.date(), // e.g. 2024-09-17
+
+    authorContact: z.string().email(),
+    canonicalURL: z.string().url(),
+    slug: z.string().default(""),
+  }),
+});
+
+export const collections = { blog };
