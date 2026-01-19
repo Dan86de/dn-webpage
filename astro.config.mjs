7453a224eff38a38343701ad6ab3d10381dea6c6
Daniel Noworyta <daniel.noworyta@gmail.com>
2026-01-19 18:47:36 +0100

    [US-001] - Configure MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "@astrojs/vercel";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 export default defineConfig({
+  integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
   },
